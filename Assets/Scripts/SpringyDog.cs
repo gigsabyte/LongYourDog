9455d659ec43f27366427e6ab9b2cb6fe3a88feb
Seiji Emery <seijiemery@gmail.com>
2019-10-17 13:41:05 -0700

    spring stuff (WIP)

--- Assets/Scripts/SpringyDog.cs
+++ Assets/Scripts/SpringyDog.cs
@@ -7,8 +7,17 @@ public class SpringyDog : MonoBehaviour {
     public GameObject segmentPrefab;
     public List<GameObject> segments;
     public int numSegments = 10;
+    public float springForce = 100.0f;
+    public float springDamp = 0.2f;
+
+    private void addSpring(GameObject obj, Rigidbody target) {
+        var sf = obj.AddComponent<SpringJoint>();
+        sf.connectedBody = target;
+        sf.spring = springForce;
+        sf.damper = springDamp;
+    }
     void Start() {
-        // spawn body segements
+        // spawn body segements + add springs
         Rigidbody prev = heads[0].GetComponent<Rigidbody>();
         for (int i = 0; i < numSegments; ++i) {
             var t = (float)(i + 1) / (float) (numSegments + 1);
@@ -16,13 +25,14 @@ public class SpringyDog : MonoBehaviour {
                 heads[1].transform.position, t);
             var rot = Quaternion.Slerp(heads[0].transform.rotation,
                 heads[1].transform.rotation, t);
-            var go = GameObject.Instantiate(segmentPrefab, pos, rot);
-            var rigidbody = go.AddComponent<Rigidbody>();
-            var sf = go.AddComponent<SpringJoint>();
-            sf.connectedBody = prev;
+            var obj = GameObject.Instantiate(segmentPrefab, pos, rot);
+            var rigidbody = obj.AddComponent<Rigidbody>();
+            rigidbody.freezeRotation = true;
+            addSpring(obj, prev);
             prev = rigidbody;
-            segments.Add(go);
+            segments.Add(obj);
         }
+        addSpring(heads[1], prev);
     }
     
     // Update is called once per frame
